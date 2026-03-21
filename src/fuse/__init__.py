d1efe66fccaeaf96b576899c39d8ff7ddf5b8baa
Sandeep Selvaraj <sandeep.selvaraj@elevait.de>
2026-03-21 13:36:53 +0100

    build: use semvar

--- src/fuse/__init__.py
+++ src/fuse/__init__.py
@@ -1,11 +1,13 @@
 """Fuse: Train small LLMs and deploy them for fast structured extraction on CPU."""
 
+from fuse._version import __version__
 from fuse.config import ExportConfig, ExtractConfig, InferenceConfig, TrainConfig
 from fuse.extraction.extractor import Extractor
 from fuse.extraction.schema import SchemaBuilder
 from fuse.inference.llama_cpp import LlamaCppBackend
 
 __all__ = [
+    "__version__",
     "ExportConfig",
     "ExtractConfig",
     "Extractor",
